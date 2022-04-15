5e682aebd18844da08ad6aa3928fd132862e7232
healtheefart <961210eee@naver.com>
2022-04-15 19:58:56 +0900

    🛠Fix: 22/04/15 PG방금그곡.java

--- Java/Programmers/모든문제/PG방금그곡.java
+++ Java/Programmers/모든문제/PG방금그곡.java
@@ -1,69 +1,55 @@
 package 모든문제;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
 public class PG방금그곡 {
-
-	static List<String> fullLyrics = new ArrayList<>();
-	static List<String> names = new ArrayList<>();
-	static int answerIdx = -1;
+	static String memory;
+	static String answer = "";
+	static Integer maxPlayT = 0;
 	public static String solution(String m, String[] musicinfos) {
-		String trimM = trim(m);
-		Arrays.stream(musicinfos).forEach(str -> complete(str, trimM));
-		findAnswer(trimM);
-		return answerIdx == -1 ? "(None)" : names.get(answerIdx);
+		memory = trim(m);
+		for (String musicinfo : musicinfos) {
+			findAnswer(musicinfo);
+		}
+		return answer.equals("") ? "(None)" : answer;
 	}
 
 	private static String trim(String m) {
 		return m.replaceAll("C#", "3")
 				.replaceAll("D#", "4")
 				.replaceAll("F#", "5")
+				.replaceAll("G#", "6")
 				.replaceAll("A#", "1");
 	}
 
-	private static void findAnswer(String m) {
-		for (int i = 0; i < fullLyrics.size(); i++) {
-			String lyric = fullLyrics.get(i);
-			if (lyric.contains(m)) {
-				if (answerIdx == -1) {
-					answerIdx = i;
-				} else {
-					if (names.get(answerIdx).length() < names.get(i).length()) {
-						answerIdx = i;
-					}
-				}
-			}
+	private static void findAnswer(String str) {
+		String[] split = str.split(",");
+
+		Integer playT = calculateT(split[0].split(":"), split[1].split(":"));
+		String name = split[2];
+
+		String[] lyrics = trim(split[3]).split("");
+		StringBuilder fullLyric = new StringBuilder("");
+
+		for (int i = 0; i < playT; i++) {
+			fullLyric.append(lyrics[i % lyrics.length]);
+		}
+
+		if (fullLyric.toString().contains(memory) && playT > maxPlayT) {
+			answer = name;
+			maxPlayT = playT;
 		}
 	}
 
-	private static void complete(String str, String m) {
-		String[] split = str.split(",");
-		String[] start = split[0].split(":");
+	private static Integer calculateT(String[] start, String[] end) {
 		Integer startT = Integer.parseInt(start[0]) * 60 + Integer.parseInt(start[1]);
-		String[] end = split[1].split(":");
 		Integer endT = Integer.parseInt(end[0]) * 60 + Integer.parseInt(end[1]);
-		Integer time = endT - startT;
-		String name = split[2];
-		String[] lyrics = trim(split[3]).split("");
-
-		StringBuilder sb = new StringBuilder();
-		System.out.println(time);
-		while (time > 0) {
-			sb.append(lyrics[sb.length() % lyrics.length]);
-			time--;
-		}
-
-		fullLyrics.add(sb.toString());
-		names.add(name);
+		return endT - startT;
 	}
 
 	public static void main(String[] args) {
 //		String m = "ABCDEFG";
-		String m = "CDEFGAC";
+		String m = "ABCDE";
 //		String[] musicinfos = {"12:00,12:14,HELLO,CDEFGAB", "13:00,13:05,WORLD,ABCDEF"};
-		String[] musicinfos = {"12:00,12:06,HELLO,CDEFGA"};
+		String[] musicinfos = {"03:00,03:05,FOOOO,ABCDEF", "03:00,03:05,HELLO,ABCDEF"};
 		System.out.println(solution(m, musicinfos));
 	}
 }
